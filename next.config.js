82b250c3146f6fe6f3607ddc936dd76ccf4dc512
truonggiang <trantruonggiang2810@gmail.com>
2023-08-14 22:32:21 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export' }
+const nextConfig = {}
 
 module.exports = {
+  output: {
+    export: true,
+  },
   images: {
     domains: ['www.themealdb.com'],
   },
